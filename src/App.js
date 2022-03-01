db590a74fb734e888182c0b674302a306c6cdc6d
unknown <amaan.patel@radiantcloud.co>
2022-03-01 23:51:33 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+     <h1>Navbar Frontend</h1>
+     <button>Normal Button</button>
+     <Button>Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
